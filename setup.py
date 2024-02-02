8380a2a02be06032ff4fa08366851c6f4ce0cb06
Carlos Suárez <bitseater@gmail.com>
2024-02-02 21:34:39 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -1,24 +1,33 @@
 import sys
 from cx_Freeze import setup, Executable
 
+base = "Win32GUI" if sys.platform == "win32" else None
+
+version = "0.1"
+
+include_files = ["data/style.css",
+                 "data/pygprix.ico"]
+
+zip_include_packages = ["PyQt6",
+                        "PySide6"]
+
 options = {
     "build_exe": {
-        "zip_include_packages": ["PyQt6", "PySide6"],
-        "include_files": ["data/style.css","data/pygprix.ico"],
+        "build_exe": "build/pygprix-" + version,
+        "zip_include_packages": zip_include_packages,
+        "include_files": include_files,
     },
 }
 
-base = "Win32GUI" if sys.platform == "win32" else None
-
-executables = [Executable("pygprix/app.py",
+executables = [Executable("src/app.py",
                           target_name="PyGPrix",
                           base=base, 
                           copyright="Copyright (C) 2024 Carlos Suárez", 
-                          icon='data/pygprix.ico',)]
+                          icon='data/pygprix.ico')]
 
 setup(
     name="PyGPrix",
-    version="0.1",
+    version=version,
     description="Ergast API with Python & QT",
     author="Carlos Suárez",
     author_email="bitseater@gmail.com",
